e02ef35dea6dbbc4ab2435fb4b11138fc74199d2
Francis <fchabouis@gmail.com>
2018-01-16 16:00:23 +0100

    aide css

--- src/components/Aide.vue
+++ src/components/Aide.vue
@@ -1,43 +1,43 @@
 <template>
     <div>
-        <v-container>
+        <v-container style="padding: 10px;">
             <h1 class="pt-5 pb-3">À propos</h1>
-            <div>
-                Airesdejeux.org a pour but de référencer toutes les aires de jeux pour enfants de France. Et de permettre aux parents d'en découvrir des nouvelles. Son fonctionnement est collaboratif.
-            </div>
-            <br>
-            <div>
+            <p>
+                Airesdejeux.org a pour but de référencer toutes les aires de jeux pour enfants de France et de permettre aux parents d'en découvrir des nouvelles.
+            </p>
+            <p>
+                Son fonctionnement est collaboratif.
+            </p>
+            <p>
                 La base initiale des aires a été extraite du projet
                 <a href="https://www.openstreetmap.org">OpenStreetMap</a>, une initiative open source et collaborative de cartographie. Il vous est possible de compléter ces informations en renseignant la liste des équipements présents, en donnant votre avis sur l'aire et en ajoutant des photos.
-            </div>
-            <br>
-            <div>
+            </p>
+            <p>
                 Le site n'a pas de vocation commerciale. En fonction de son succès, il sera possible d'imaginer faire quelque chose des informations recueillies. Comme par exemple de collaborer avec les mairies pour leur signaler un équipement défectueux et leur faire parvenir des suggestions.
-            </div>
-            <br>
-            <div>
-                Pour ceux qui cela intéresse, le code du projet est libre et est publié sur github.
-            </div>
+            </p>
+            <p>
+                Pour ceux que cela intéresse, le code du projet est libre et est publié sur github.
+            </p>
 
             <h1 class="pt-5">FAQ</h1>
-            <div class="pt-4 title">
+            <p class="pt-4 title">
                 Comment puis-je participer ?
-            </div>
-            <div>
+            </p>
+            <p>
                 Il vous faut créer un compte. Vous pourrez ensuite noter les aires et compléter les informations.
-            </div>
-            <div class="pt-4 title">
+            </p>
+            <p class="pt-4 title">
                 Est-il possible d'ajouter une aire ?
-            </div>
-            <div>
+            </p>
+            <p>
                 Si une aire n'existe pas sur la carte et que vous souhaitez l'ajouter, faites un clic droit sur la carte (sur ordinateur) ou un appui long (sur mobile et tablette).
-            </div>
-            <div class="pt-4 title">
+            </p>
+            <p class="pt-4 title">
                 Qui est derrière ce projet ?
-            </div>
-            <div>
+            </p>
+            <p>
                 Francis Chabouis. Je suis un ingénieur et développeur freelance à Paris avec un petit faible pour les cartes.
-            </div>
+            </p>
 
         </v-container>
     </div>
