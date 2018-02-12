7c573b404dc9f60294384e8866b5ce92b50a9ba5
Francis <fchabouis@gmail.com>
2018-02-12 15:07:38 +0100

    a click on user name sends back to the map

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
               <v-icon light>person</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>{{ displayName }}</v-list-tile-title>
+              <v-list-tile-title @click='home'>{{ displayName }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-list-tile>
