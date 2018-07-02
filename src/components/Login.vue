5fac25d04d876296f47396f0f68ba79f741c3cc9
Francis <fchabouis@gmail.com>
2018-07-02 11:33:30 +0200

    enable facebook login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -18,7 +18,7 @@ let uiConfig = {
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
