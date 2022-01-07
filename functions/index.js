fea30934e8effc56e4396fda7e41e1825e9acb15
Francis Chabouis <fchabouis@gmail.com>
2022-01-07 12:13:28 +0100

    update thumbnail function
    according to latest example given https://github.com/firebase/functions-samples/blob/main/generate-thumbnail/functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -16,11 +16,9 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const mkdirp = require('mkdirp-promise');
-// Include a Service Account Key to use a Signed URL
-const gcs = require('@google-cloud/storage')({keyFilename: 'service-account-credentials.json'});
+const mkdirp = require('mkdirp');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
@@ -38,11 +36,11 @@ const THUMB_PREFIX = 'thumb_';
  * After the thumbnail has been generated and uploaded to Cloud Storage,
  * we write the public URL to the Firebase Realtime Database.
  */
-exports.generateThumbnail = functions.storage.object().onChange(event => {
+exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
   // File and directory paths.
-  console.log('changement : ' + event.data.name)
-  const filePath = event.data.name;
-  const contentType = event.data.contentType; // This is the image Mimme type
+  console.log('changement : ' + object.name)
+  const filePath = object.name;
+  const contentType = object.contentType; // This is the image Mimme type
   const fileDir = path.dirname(filePath);
   const fileName = path.basename(filePath);
   const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
@@ -57,62 +55,57 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
 
   // Exit if this is triggered on a file that is not an image.
   if (!contentType.startsWith('image/')) {
-    console.log('This is not an image.');
-    return null;
+    return functions.logger.log('This is not an image.');
   }
 
   // Exit if the image is already a thumbnail.
   if (fileName.startsWith(THUMB_PREFIX)) {
-    console.log('Already a Thumbnail.');
-    return null;
+    return functions.logger.log('Already a Thumbnail.');
   }
 
   // Exit if this is a move or deletion event.
-  if (event.data.resourceState === 'not_exists') {
+  // note : not sure if still necessary
+  if (object.resourceState === 'not_exists') {
     console.log('This is a deletion event.');
     return null;
   }
 
   // Cloud Storage files.
-  const bucket = gcs.bucket(event.data.bucket);
+  const bucket = admin.storage().bucket(object.bucket);
   const file = bucket.file(filePath);
   const thumbFile = bucket.file(thumbFilePath);
-  const metadata = { contentType: contentType };
+  const metadata = { contentType: contentType, 'Cache-Control': 'public,max-age=3600'};
 
   // Create the temp directory where the storage file will be downloaded.
-  return mkdirp(tempLocalDir).then(() => {
-    // Download file from bucket.
-    return file.download({destination: tempLocalFile});
-  }).then(() => {
-    console.log('The file has been downloaded to', tempLocalFile);
-    // Generate a thumbnail using ImageMagick.
-    return spawn('convert', [tempLocalFile, '-auto-orient', '-normalize', '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile], {capture: ['stdout', 'stderr']});
-  }).then(() => {
-    console.log('Thumbnail created at', tempLocalThumbFile);
-    // Uploading the Thumbnail.
-    return bucket.upload(tempLocalThumbFile, { destination: thumbFilePath, metadata: metadata });
-  }).then(() => {
-    console.log('Thumbnail uploaded to Storage at', thumbFilePath);
-    // Once the image has been uploaded delete the local files to free up disk space.
-    fs.unlinkSync(tempLocalFile);
-    fs.unlinkSync(tempLocalThumbFile);
-    // Get the Signed URLs for the thumbnail and original image.
-    const config = {
-      action: 'read',
-      expires: '03-01-2500'
-    };
-    return Promise.all([
-      thumbFile.getSignedUrl(config),
-      file.getSignedUrl(config)
-    ]);
-  }).then(results => {
-    console.log('Got Signed URLs.');
-    const thumbResult = results[0];
-    const originalResult = results[1];
-    const thumbFileUrl = thumbResult[0];
-    const fileUrl = originalResult[0];
-    // Add the URLs to the Database
-    console.log('la fonction a été appelée')
-    return admin.database().ref('images/' + aire + '/' + pictureId).set({path: fileUrl, thumbnail: thumbFileUrl, verified: false, uid: uid});
-  }).then(() => console.log('Thumbnail URLs saved to database.'));
+  await mkdirp(tempLocalDir)
+  // Download file from bucket.
+  await file.download({destination: tempLocalFile});
+  functions.logger.log('The file has been downloaded to', tempLocalFile);
+  // Generate a thumbnail using ImageMagick.
+  await spawn('convert', [tempLocalFile, '-auto-orient', '-normalize', '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile], {capture: ['stdout', 'stderr']});
+  console.log('Thumbnail created at', tempLocalThumbFile);
+  // Uploading the Thumbnail.
+  await bucket.upload(tempLocalThumbFile, { destination: thumbFilePath, metadata: metadata });
+  console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+  fs.unlinkSync(tempLocalFile);
+  fs.unlinkSync(tempLocalThumbFile);
+
+  // Get the Signed URLs for the thumbnail and original image.
+  const config = {
+    action: 'read',
+    expires: '03-01-2500'
+  }
+  return Promise.all([
+    thumbFile.getSignedUrl(config),
+    file.getSignedUrl(config)
+  ]);
+  console.log('Got Signed URLs.');
+  const thumbResult = results[0];
+  const originalResult = results[1];
+  const thumbFileUrl = thumbResult[0];
+  const fileUrl = originalResult[0];
+  // Add the URLs to the Database
+  await admin.database().ref('images/' + aire + '/' + pictureId).set({path: fileUrl, thumbnail: thumbFileUrl, verified: false, uid: uid});
+  console.log('la fonction a été appelée')
+  return functions.logger.log('Thumbnail URLs saved to database.');
 });
