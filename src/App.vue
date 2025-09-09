7c8a463f35f3824e5d19a2cedf7d925709285959
Francis Chabouis <fchabouis@gmail.com>
2025-09-09 08:13:52 +0200

    remove api keys from public repo

--- src/App.vue
+++ src/App.vue
@@ -96,12 +96,12 @@ import firebase from 'firebase/compat/app'
 
 // Initialize Firebase
 let config = {
-  apiKey: 'AIzaSyCQex4JKbyUpZHggelHR9INIIAiMetm6DQ',
-  authDomain: 'aires-de-jeux-e8525.firebaseapp.com',
-  databaseURL: 'https://aires-de-jeux-e8525.firebaseio.com',
-  projectId: 'aires-de-jeux-e8525',
-  storageBucket: 'aires-de-jeux-e8525.appspot.com',
-  messagingSenderId: '105193380802'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
